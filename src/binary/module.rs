9f419447a4356d4a27c1e4d0c15c1d51f3132a3d
quantumshiro <hiro.1nakanishi@gmail.com>
2024-05-05 15:51:33 +0900

    fix: change directory structure

--- src/binary/module.rs
+++ src/binary/module.rs
@@ -1,5 +1,49 @@
+use nom::{IResult, number::complete::le_u32, bytes::complete::tag};
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct Module {
     pub magic: String,
     pub version: u32,
+}
+
+impl Default for Module {
+    fn default() -> Self {
+        Self {
+            magic: "\0asm".to_string(),
+            version: 1,
+        }
+    }
+}
+
+impl Module {
+    pub fn new(input: &[u8]) -> anyhow::Result<Module> {
+        let (_, module) = Module::decode(input).map_err(|_| anyhow::anyhow!("Failed to decode module"))?;
+        Ok(module)
+    }
+
+    fn decode(input: &[u8]) -> IResult<&[u8], Module> {
+        let (input, _) = tag(b"\0asm")(input)?;
+        let (input, version) = le_u32(input)?;
+
+        let module = Module {
+            magic: "\0asm".into(),
+            version,
+        };
+        Ok((input, module))
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::binary::module::Module;
+    use anyhow::Result;
+
+    #[test]
+    fn decode_module() -> Result<()> {
+        let wasm = wat::parse_str("(module)")?;
+        let module = Module::new(&wasm)?;
+        assert_eq!(module, Module::default());
+        Ok(())
+    }
 }
