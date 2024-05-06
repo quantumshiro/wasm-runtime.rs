fd300508a7e61582484d2cded64048b90d5f3f3c
quantumshiro <hiro.1nakanishi@gmail.com>
2024-05-06 23:15:36 +0900

    feat: add new Store constructor for initializing with a Module

--- src/execution/store.rs
+++ src/execution/store.rs
@@ -1,7 +1,9 @@
 use crate::binary::{
     instruction::Instruction,
     types::{FuncType, ValueType},
+    module::Module,
 };
+use anyhow::{bail, Result};
 
 #[derive(Clone)]
 pub struct Func {
@@ -23,4 +25,44 @@ pub enum FuncInst {
 #[derive(Default)]
 pub struct Store {
     pub funcs: Vec<FuncInst>,
+}
+
+impl Store {
+    pub fn new(module: Module) -> Result<Self> {
+        let func_type_indexs = match module.function_section {
+            Some(ref indexs) => indexs.clone(),
+            _ => vec![],
+        };
+
+        let mut funcs = vec![];
+
+        if let Some(ref code_section) = module.code_section {
+            for (func_body, type_idx) in code_section.iter().zip(func_type_idxs.into_iter()) {
+                let Some(ref func_types) = module.type_section else {
+                    bail!("not found type_section")
+                };
+
+                let Some(func_type) = func_types.get(type_idx as usize) else {
+                    bail!("not found func type in type_section")
+                };
+
+                let mut locals = Vec::with_capacity(func_body.locals.len());
+                for local in func_body.locals.iter() {
+                    for _ in 0..local.type_count {
+                        locals.push(local.value_type.clone());
+                    }
+                }
+
+                let func = FuncInst::Internal(InternalFuncInst {
+                    func_type: func_type.clone(),
+                    code: Func {
+                        locals,
+                        body: func_body.code.clone(),
+                    },
+                });
+                funcs.push(func);
+            }
+        }
+        Ok(Store { funcs })
+    }
 }
